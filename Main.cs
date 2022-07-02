dcee1c6116ba3a2cd3c7bda2a2a0a3a99f842f65
MrPurple6411 <mrpurple6411@gmail.com>
2022-07-02 15:51:47 -0600

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,26 @@
+﻿namespace SkipIntro
+{
+    using SkipIntro.Settings;
+
+    public class Main: BaseManager
+    {
+        internal static readonly SettingsCategory SettingsCategory;
+        static Main()
+        {
+            SettingsCategory = Manager<SettingsManager>.Instance.globalSettingsCategories.Find(x => x.id == "MrPurple6411_Settings");
+            if (SettingsCategory == null)
+            {
+                SettingsCategory = new SettingsCategory(new OctDatGlobalInitializer())
+                {
+                    id = "MrPurple6411_Settings",
+                    name = "MrPurple's Mod Settings",
+                    canReset = true,
+                    global = true,
+                    order = -1000
+                };
+                SettingsCategory.PostInit();
+            }
+            SettingsCategory.AddSetting(new SkipIntroSetting(new OctDatGlobalInitializer()));
+        }
+    }
+}
---
+++ Settings/Settings.cs
@@ -0,0 +1,27 @@
+﻿namespace SkipIntro.Settings
+{
+    using UnityEngine;
+
+    public class SkipIntroSetting : ToggleSettingDefinition
+    {
+        public SkipIntroSetting(OctDatGlobalInitializer initializer) : base(initializer)
+        {
+            this.id = "Oct.Settings.MySettings.SkipIntro";
+            this.name = "Skip Intro";
+            this.order = Main.SettingsCategory.settings.Count;
+            this.category = Main.SettingsCategory;
+            this.defaultValue = true;
+        }
+
+        public override void Apply()
+        {
+            KingdomManager.SkipIntro = (bool)this.GetValue();
+            base.Apply();
+        }
+
+        public override Color LabelColor()
+        {
+            return Color.magenta;
+        }
+    }
+}
